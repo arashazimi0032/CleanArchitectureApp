42d8f3cccc76934e7e77fcf467779fa5647837f1
arashazimi0032 <arashazimi0032@gmail.com>
2023-10-08 23:48:32 +0330

    Commit Service and repository Interfaces, CourseViewModel

---
+++ CleanArchitectureSample/CleanArchitectureSample.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureSample.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureSample.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
---
+++ CleanArchitectureSample/CleanArchitectureSample.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureSample.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureSample.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses();
+    }
+}
