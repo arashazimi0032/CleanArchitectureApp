b49d4be3c863b6b906fbba882b2882601b151712
arashazimi0032 <arashazimi0032@gmail.com>
2023-10-08 21:39:58 +0330

    update connectionString and add migration

--- CleanArchitectureSample/CleanArchitectureSample.Mvc/Data/Migrations/20231008175803_InitialMigration.Designer.cs
+++ CleanArchitectureSample/CleanArchitectureSample.Mvc/Data/Migrations/20231008175803_InitialMigration.Designer.cs
@@ -4,6 +4,7 @@ using CleanArchitectureSample.Mvc.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchitectureSample.Mvc.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231008175803_InitialMigration")]
+    partial class InitialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
