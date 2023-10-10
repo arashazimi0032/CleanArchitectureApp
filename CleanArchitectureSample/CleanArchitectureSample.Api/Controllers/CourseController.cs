302358c7b3891fddf0b5972f80031324a0165c35
arashazimi0032 <arashazimi0032@gmail.com>
2023-10-10 21:32:24 +0330

    add api project and rename controller

---
+++ CleanArchitectureSample/CleanArchitectureSample.Api/Controllers/CourseController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureSample.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+    }
+}
